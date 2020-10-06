911d23cc9f67d23f8def7590bde3e27688ed649b
Amakoye <charlesamakoye@gmail.com>
2020-10-06 11:23:09 +0300

    added navigation  using Angular Routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from "@angular/platform-browser";
+import { RouterModule, Routes } from "@angular/router";
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { HttpModule } from "@angular/http";
@@ -8,8 +9,18 @@ import { ProductPageComponent } from "./product-page/product-page.component";
 import { ProductDescriptionComponent } from "./product-description/product-description.component";
 
 import { ProductService } from "./product.service";
-import { ProductTracklistingComponent } from './product-tracklisting/product-tracklisting.component';
-import { ProductListComponent } from './product-list/product-list.component';
+import { ProductTracklistingComponent } from "./product-tracklisting/product-tracklisting.component";
+import { ProductListComponent } from "./product-list/product-list.component";
+
+const appRoutes: Routes = [
+  { path: "products", component: ProductListComponent },
+  { path: "product/:id", component: ProductPageComponent },
+  {
+    path: "",
+    redirectTo: "products",
+    pathMatch: "full",
+  },
+];
 
 @NgModule({
   declarations: [
@@ -19,7 +30,12 @@ import { ProductListComponent } from './product-list/product-list.component';
     ProductTracklistingComponent,
     ProductListComponent,
   ],
-  imports: [BrowserModule, FormsModule, HttpModule],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot(appRoutes),
+  ],
   providers: [ProductService],
   bootstrap: [AppComponent],
 })
